fa0e841f4d9207f561ae3c7a67d105a208ecbe30
Viktor Tanyi <tanyi.viktor@gmail.com>
2014-12-25 15:50:10 +0100

    Add two-dimensional movement and bouncing

--- Main.hs
+++ Main.hs
@@ -3,20 +3,25 @@
 module Main where
 
 import Prelude hiding (id, (.))
+import Control.Monad.Zip (mzipWith)
 import Control.Wire
 import Data.IORef
+import Data.Maybe (fromJust, isJust)
 import qualified Graphics.Gloss as Gloss
 import qualified Graphics.Gloss.Interface.IO.Animate as Gloss
+import Linear
 
 
 type W a b = Wire (Timed Float ()) () Identity a b
 
-type Ball = (Float, Float)
+type Ball = (V2 Float, V2 Float)
 
 mainWire :: W () Gloss.Picture
 mainWire = proc () -> do
-  (pos, _vel) <- ballWire -< ()
-  returnA -< Gloss.translate pos 0 $ Gloss.circle 10
+  (V2 px py, V2 vx vy) <- ballWire -< ()
+  let circ    = Gloss.circle 10
+      velLine = Gloss.line [(0, 0), (vx, vy)]
+  returnA -< Gloss.translate px py . Gloss.pictures $ [circ, velLine]
 
 ballWire :: W () Ball
 ballWire = proc () -> do
@@ -25,18 +30,38 @@ ballWire = proc () -> do
     ball <- moveAndMaybeBounce -< ball'
   returnA -< ball
   where
-    ballInit = (0, 1)
+    ballInit = (0, V2 3 2)
     moveAndMaybeBounce = (bounceBall <<< moveBall) <|> moveBall
 
 moveBall :: W Ball Ball
 moveBall = arr $ \(pos, vel) -> (pos + vel, vel)
 
 bounceBall :: W Ball Ball
-bounceBall = negateVel <<< when bounceNeeded
+bounceBall = proc ball@(pos, vel) -> do
+  normal <- arrJust bounceNormal -< ball
+  reflectedVel <- arrJust $ uncurry reflectIfNeeded -< (vel, normal)
+  returnA -< (pos, reflectedVel)
+
+bounceNormal :: Ball -> Maybe (V2 Float)
+bounceNormal (V2 px py, _vel)
+  | px <= -480 = Just $ V2   1   0
+  | px >=  480 = Just $ V2 (-1)  0
+  | py <= -270 = Just $ V2   0   1
+  | py >=  270 = Just $ V2   0 (-1)
+  | otherwise  = Nothing
+
+reflectIfNeeded :: V2 Float -> V2 Float -> Maybe (V2 Float)
+reflectIfNeeded vel normal
+  | vel `dot` normal < 0 = Just $ mzipWith negateVelIfNormalNonzero vel normal
+  | otherwise            = Nothing
   where
-    negateVel = arr $ \(pos, vel) -> (pos, -vel)
-    bounceNeeded (pos, vel) =
-      (pos <= -480 && vel < 0) || (pos >= 480 && vel > 0)
+    negateVelIfNormalNonzero vi ni
+      | ni /= 0   = -vi
+      | otherwise =  vi
+
+
+arrJust :: (Monoid e, Monad m) => (a -> Maybe b) -> Wire s e m a b
+arrJust f = arr fromJust <<< when isJust <<< arr f
 
 main :: IO ()
 main = do
