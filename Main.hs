5123895c8239a86ee82d988faad415fda5f45d02
Viktor Tanyi <tanyi.viktor@gmail.com>
2015-01-02 14:07:46 +0100

    Add a destroyable brick

--- Main.hs
+++ Main.hs
@@ -19,44 +19,59 @@ import Linear
 type W a b = Wire.Wire (Wire.Timed Float ()) () (Reader Float) a b
 
 type Ball = (V2 Float, V2 Float)
+type Brick = (V2 Float, Float)
+type State = (Ball, Maybe Brick)
 
 mainWire :: W () Gloss.Picture
 mainWire = proc () -> do
-  (V2 px py, V2 vx vy) <- ballWire -< ()
+  ((V2 px py, V2 vx vy), maybeBrick) <- gameWire -< ()
   mouseX <- Wire.mkGen_ (const (Right <$> ask)) -< ()
   let circ    = Gloss.circle ballRadius
       velLine = Gloss.line [(0, 0), (vx, vy)]
       ballPic = Gloss.translate px py . Gloss.pictures $ [circ, velLine]
       batPic  = Gloss.translate mouseX batPositionY
               $ Gloss.rectangleWire batWidth batHeight
-  returnA -< Gloss.pictures [ballPic, batPic]
+      brickPic =
+        case maybeBrick of
+          Just (V2 bx by, r) -> Gloss.translate bx by $ Gloss.circle r
+          Nothing            -> Gloss.blank
+  returnA -< Gloss.pictures [ballPic, batPic, brickPic]
 
-ballWire :: W () Ball
-ballWire = proc () -> do
+gameWire :: W () State
+gameWire = proc () -> do
   rec
-    ball' <- Wire.delay ballInit -< ball
-    ball <- updateBall -< ball'
-  returnA -< ball
+    state' <- Wire.delay stateInit -< state
+    state <- updateGame -< state'
+  returnA -< state
 
-updateBall :: W Ball Ball
-updateBall =
+updateGame :: W State State
+updateGame =
   moveBall
-  ^>> ballEdgeCollisionWire
-  <|> ballBatCollisionWire
-  <|> Wire.when ballAlive
-  <|> arr (const ballInit)
+  ^>> collideBallBrick
+  <|> first collideBallEdge
+  <|> first collideBallBat
+  <|> first (Wire.when ballAlive)
+  <|> pure stateInit
   where
-    moveBall (pos, vel) = (pos + vel, vel)
+    moveBall ((pos, vel), maybeBrick) = ((pos + vel, vel), maybeBrick)
     ballAlive (V2 _px py, _vel) = py > screenLowerBound
 
-ballEdgeCollisionWire :: W Ball Ball
-ballEdgeCollisionWire = Wire.mkPure_ $ \ball@(pos, _vel) ->
+collideBallEdge :: W Ball Ball
+collideBallEdge = Wire.mkPure_ $ \ball@(pos, _vel) ->
   ballEdgeNormal pos >>= bounceBall ball
 
-ballBatCollisionWire :: W Ball Ball
-ballBatCollisionWire = Wire.mkGen_ $ \ball@(pos, _vel) -> do
+collideBallBat :: W Ball Ball
+collideBallBat = Wire.mkGen_ $ \ball@(pos, _vel) -> do
   batX <- ask
   return $ ballBatNormal batX pos >>= bounceBall ball
+
+collideBallBrick :: W State State
+collideBallBrick = Wire.mkPure_ $ \(ball@(pos, _vel), maybeBrick) ->
+  case maybeBrick of
+    (Just brick) -> do
+      ball' <- ballBrickNormal brick pos >>= bounceBall ball
+      return (ball', Nothing)
+    Nothing -> Left ()
 
 bounceBall :: Ball -> V2 Float -> Either () Ball
 bounceBall (pos, vel) normal
@@ -80,6 +95,13 @@ ballBatNormal batX (V2 px py)
     bxl = px >= batX - batWidth / 2
     bxr = px <= batX + batWidth / 2
     by  = py <= batPositionY + batHeight / 2 + ballRadius
+
+ballBrickNormal :: Brick -> V2 Float -> Either () (V2 Float)
+ballBrickNormal (brickPos, brickRadius) pos
+  | hit       = Right . normalize $ pos - brickPos
+  | otherwise = Left ()
+  where
+    hit = distance brickPos pos <= brickRadius + ballRadius
 
 batNormal :: Float -> Float -> V2 Float
 batNormal x batX = perp . angle $ batSpread * relX
@@ -136,9 +158,6 @@ screenLowerBound = (-screenUpperBound)
 ballRadius :: Float
 ballRadius = 10
 
-ballInit :: Ball
-ballInit = (0, V2 0 (-5))
-
 batWidth :: Float
 batWidth = 160
 
@@ -150,3 +169,6 @@ batPositionY = screenLowerBound + batHeight / 2
 
 batSpread :: Float
 batSpread = pi / 6
+
+stateInit :: State
+stateInit = ((0, V2 0 (-5)), Just (V2 100 100, 20))
